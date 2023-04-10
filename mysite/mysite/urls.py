d8529e362fe60f2f564f391a4476807773c72b20
srazam <srazam5@gmail.com>
2023-04-10 13:03:05 -0400

    Tried changing the urls.py and settings.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('main.urls')),
     path('login/', include('main.urls')),
-    path('home', include('main.urls')),
+    path('home/', include('main.urls')),
 ]
